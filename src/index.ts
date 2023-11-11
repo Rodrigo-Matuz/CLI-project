bed1eebd0346402bad26a4bc56b9e4ba9ecbd62a
Matuz <rodrigosantoslima1311@gmail.com>
2023-11-11 10:07:00 -0300

    commander library set

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
 #!/usr/bin/env tsx
 
-console.log("Hello World!");
+import { Command } from "commander";
+const program = new Command();
+
+program.name("ChatGPT CLI").description("CLI to access ChatGPT 4.0 Turbo");
+
+// input example:   gpt Hello
+// output:          Hello
+program
+    .description("Print back the input")
+    .argument("<string>", "Text to use as input")
+    .action((str) => {
+        console.log(str);
+    });
+
+// input example:   gpt config --df Hello
+// output:          "Hello" as default prompt
+program
+    .command("config")
+    .description("use this command to configure stuff like the default prompt")
+    .option("-df, --default-prompt", "set or change the default prompt")
+    .action((str, options) => {
+        console.log(str, options);
+    });
+
+program.parse();
